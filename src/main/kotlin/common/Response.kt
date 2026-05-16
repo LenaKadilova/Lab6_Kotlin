dd2256899efbf7db6478a5ddd2804f9b66d7ba80
Asya Zatep <asya.zatepyakina@gmail.com>
2026-05-16 15:17:56 +0300

    доп норм

--- src/main/kotlin/common/Response.kt
+++ src/main/kotlin/common/Response.kt
@@ -4,5 +4,6 @@ import java.io.Serializable
 
 data class Response(
     val message: String,
-    val lines: List<String>? = null
+    val lines: List<String>? = null,
+    val commands: Map<String, String> = emptyMap()
 ) : Serializable
